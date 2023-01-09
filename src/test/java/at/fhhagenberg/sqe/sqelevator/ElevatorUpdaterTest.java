9d4f5d714d45d5304fc3b678f51ca04e029b1de3
amdoku <aqpoba@gmail.com>
2023-01-09 16:15:10 +0100

    hiniga test

--- src/test/java/at/fhhagenberg/sqe/sqelevator/ElevatorUpdaterTest.java
+++ src/test/java/at/fhhagenberg/sqe/sqelevator/ElevatorUpdaterTest.java
@@ -1,9 +1,19 @@
 package at.fhhagenberg.sqe.sqelevator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import sqelevator.Elevator;
 import sqelevator.IElevator;
 import sqelevator.updater.ElevatorUpdater;
+
+import java.rmi.RemoteException;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ElevatorUpdaterTest {
 
@@ -14,5 +24,25 @@ public class ElevatorUpdaterTest {
         IElevator elevatorService = new MockElevatorService();
 
         ElevatorUpdater elevatorUpdater = new ElevatorUpdater(elevator, elevatorService);
+    }
+
+    @Mock
+    IElevator control = null;
+
+    @BeforeEach
+    public void Setup() {
+        control = new MockElevatorService();
+    }
+
+    @Test
+    public void test2() throws RemoteException {
+        Elevator e = new Elevator(0);
+
+        when(mock(control).getElevatorCapacity(0)).thenReturn(17);
+
+        ElevatorUpdater updater = new ElevatorUpdater(e, control);
+        Assertions.assertEquals(0, e.currentCapacity.getValue());
+        updater.Update();
+        Assertions.assertEquals(17, e.currentCapacity.getValue());
     }
 }
