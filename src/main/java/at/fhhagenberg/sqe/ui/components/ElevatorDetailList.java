f8abfe9aa434ecfc04610ba4ef00dc3d5d346c48
Fabian Dietachmair <fabian.dietachmair@icloud.com>
2023-01-16 19:31:42 +0100

    bind targetfloor to detail view

--- src/main/java/at/fhhagenberg/sqe/ui/components/ElevatorDetailList.java
+++ src/main/java/at/fhhagenberg/sqe/ui/components/ElevatorDetailList.java
@@ -13,6 +13,7 @@ public class ElevatorDetailList extends VBox{
     private Label payloadVal    = new Label();
     private Label floorPosVal = new Label();
     private Label doorStatusVal = new Label();
+    private Label targetFloorVal = new Label();
     private CheckBox autoManual;
 
 
@@ -28,10 +29,12 @@ public class ElevatorDetailList extends VBox{
         HBox doorStatusBox = new HBox(doorStatusLbl, doorStatusVal);
         Label floorPosLbl = new Label("Current floor:  ");
         HBox floorPosBox = new HBox(floorPosLbl, floorPosVal);
+        Label targetFloorLbl = new Label("Target floor:  ");
+        HBox targetFloorBox = new HBox(targetFloorLbl, targetFloorVal);
 
         autoManual = new CheckBox("Automatic mode"); //TODO: implement bind in ToggleSwitch
 
-        getChildren().addAll(autoManual, speedBox, payloadBox, doorStatusBox, floorPosBox);
+        getChildren().addAll(autoManual, speedBox, payloadBox, doorStatusBox, floorPosBox, targetFloorBox);
         listView.setOnChangedFunction(this::updateBindings);
     }
 
@@ -41,6 +44,7 @@ public class ElevatorDetailList extends VBox{
         payloadVal.textProperty().bind(Bindings.convert(elevator.currentWeight));
         doorStatusVal.textProperty().bind(Bindings.convert(elevator.door));
         floorPosVal.textProperty().bind(Bindings.convert(elevator.currentFloor));
+        targetFloorVal.textProperty().bind((Bindings.convert(elevator.targetFloor)));
         return null;
     }
 }
