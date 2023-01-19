31972e976bee55135c418b1f540fe5ab58f32351
Christian Niederer <s2110567006@fhooe.at>
2023-01-19 22:03:15 +0100

    [:snake:] maybe fix testcase

--- src/test/java/at/fhhagenberg/sqe/ui/components/ElevatorListViewTest.java
+++ src/test/java/at/fhhagenberg/sqe/ui/components/ElevatorListViewTest.java
@@ -1,16 +1,20 @@
 package at.fhhagenberg.sqe.ui.components;
 
 import at.fhhagenberg.sqe.sqelevator.mock.MockApp;
+import at.fhhagenberg.sqe.ui.view.ElevatorControlUI;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
+import org.testfx.framework.junit5.Stop;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,21 +37,59 @@ class ElevatorListViewTest {
     }
 
     @Test
-    void testReturnsFirstElevatorIfNoneSelected(FxRobot robot) {
+    void testPropertyIsNullIfNoElevatorSelected(FxRobot robot) {
         var list = robot.lookup("#elevatorlist").queryAs(ElevatorListView.class);
-        ElevatorListView.ElevatorListItem firstElement = robot.lookup("#elevatorlist").queryAs(ElevatorListView.class).elevatorList.getItems().get(0);
-        ElevatorListView.ElevatorListItem secondElement = robot.lookup("#elevatorlist").queryAs(ElevatorListView.class).elevatorList.getItems().get(1);
 
         assertNull(list.currentElevatorProperty.get());
+    }
 
-        robot.clickOn("#elevatorlist > #elevatorlistview #elevator_0", MouseButton.PRIMARY);
-        assertEquals(firstElement.e, list.currentElevatorProperty.get().e);
+    @Test
+    void testListIsNotEditable(FxRobot robot) {
+        var list = robot.lookup("#elevatorlist").queryAs(ElevatorListView.class);
+
+        assertFalse(list.elevatorList.isEditable());
+    }
+
+    @Test
+    void testElevatorLabelText(FxRobot robot) {
+        ElevatorListView.ElevatorListItem firstElement = robot.lookup("#elevatorlist > #elevatorlistview #elevator_0").queryAs(ElevatorListView.ElevatorListItem.class);
+        ElevatorListView.ElevatorListItem secondElement = robot.lookup("#elevatorlist > #elevatorlistview #elevator_1").queryAs(ElevatorListView.ElevatorListItem.class);
 
         assertEquals("Elevator 0", firstElement.getText());
         assertEquals("Elevator 1", secondElement.getText());
+    }
 
-        robot.clickOn("#elevatorlist > #elevatorlistview #elevator_1", MouseButton.PRIMARY);
-        assertFalse(list.elevatorList.isEditable());
-        assertEquals(secondElement.e, list.currentElevatorProperty.get().e);
+    @Test
+    void testReturnsSelectedElevator(FxRobot robot) {
+        var controlui = robot.lookup("#elevatorcontrolui").queryAs(ElevatorControlUI.class);
+        var list = robot.lookup("#elevatorlist").queryAs(ElevatorListView.class);
+        ElevatorListView.ElevatorListItem firstElement = robot.lookup("#elevatorlist > #elevatorlistview #elevator_0").queryAs(ElevatorListView.ElevatorListItem.class);
+        ElevatorListView.ElevatorListItem secondElement = robot.lookup("#elevatorlist > #elevatorlistview #elevator_1").queryAs(ElevatorListView.ElevatorListItem.class);
+
+        waitForUpdate();
+        Assumptions.assumeTrue(list.currentElevatorProperty.get() == null);
+        waitForUpdate();
+
+        robot.clickOn("#elevatorlist > #elevatorlistview #elevator_0", MouseButton.PRIMARY)
+            .interact(controlui::updateElevators)
+            .interact(this::waitForUpdate)
+            .interact(() -> assertEquals(firstElement.e.elevatorNumber, list.currentElevatorProperty.get().e.elevatorNumber))
+            .clickOn("#elevatorlist > #elevatorlistview #elevator_1", MouseButton.PRIMARY)
+            .interact(this::waitForUpdate)
+            .interact(controlui::updateElevators)
+            .interact(() -> assertEquals(secondElement.e.elevatorNumber, list.currentElevatorProperty.get().e.elevatorNumber));
+    }
+
+    @Stop
+    void cleanup() throws Exception {
+        app.stop();
+    }
+
+    void waitForUpdate() {
+        try {
+            Thread.sleep(Duration.ofMillis(1500L).toMillis());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
